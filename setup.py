d803ee91e6501209bcde95008402b038cfa2dfe0
Ben Demaree <bendemaree@gmail.com>
2023-03-23 14:41:20 -0500

    Drop unused dependency

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         'setuptools',
         'intuit-oauth==1.2.3',
         'rauth>=0.7.1',
-        'authclient',
         'requests>=2.19.1',
         'simplejson>=3.17.0',
         'six>=1.14.0',
